df85bc8c2dff4e5528f69bf86531c786d5bab0a5
Michael Park <thatmichaelpark@gmail.com>
2018-02-03 23:25:09 -0800

    master same token

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Aloha omg master!
+          Aloha omg master w/same token!
         </p>
       </div>
     );
